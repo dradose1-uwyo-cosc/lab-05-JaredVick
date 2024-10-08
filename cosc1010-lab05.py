c2105ac190376d3622f3c4ec765f50355091ecac
Jared Vickrey <bvickre1@uwyo.edu>
2024-10-08 15:35:10 -0600

    Update cosc1010-lab05.py

--- cosc1010-lab05.py
+++ cosc1010-lab05.py
@@ -1,9 +1,9 @@
-# Your Name Here
+# Jared Vickrey
 # UWYO COSC 1010
 # Submission Date
 # Lab 03 
-# Lab Section: 
-# Sources, people worked with, help given to: 
+# Lab Section: 10
+# Sources, people worked with, help given to: I asked ChatGPT for help. Chat link here: https://chatgpt.com/share/6705a364-795c-800f-8d08-84187b601b22
 # your
 # comments
 # here
@@ -94,6 +94,14 @@ min_temps = [
 # You cannot use in-built functions like max(), min() or sort the lists
 # The use of len() is fine
 # You can do this in two individual loops, or a single loop if you wish 
+max_temp = 0
+min_temp = 100
+for i in max_temps:
+    if i > max_temp:
+        max_temp = i
+for i in min_temps:
+    if i < min_temp:
+        min_temp = i
 
 print(f"Max temp = {max_temp}")
 print(f"Min temp = {min_temp}")
@@ -104,11 +112,32 @@ numbers = [-61, -76, 94, 21, 97, -4, 21, 56, -26, 9, 100, 56, -7, -32, 60, -68, 
 # You should print the number and the result within an f-string 
 # Example output: 83 is positive
 
+pos_count = 0
+neg_count = 0
+zero_count = 0
+for i in numbers:
+    if i > 0:
+        print(f"{i} is positve")
+        pos_count = pos_count + 1
+    elif i < 0:
+        print(f"{i} is negative")
+        neg_count = neg_count + 1
+    elif i == 0:
+        print (f"{i} is neither positive nor negative")
+        zero_count = zero_count + 1
+
 print(f'There are {pos_count} positive numbers')
 print(f'There are {neg_count} negative numbers')
 print(f"Zero occurred {zero_count} time(s)")
 # Given the same numbers list, give the sum of all positive numbers, and the sum of all negative numbers
 # This should be done within a single loop
-
+pos_sum = 0
+for i in numbers:
+    if i > 0:
+        pos_sum = pos_sum + i
+neg_sum = 0
+for i in numbers:
+    if i < 0:
+        neg_sum = neg_sum + i
 print(f"Sum of positive numbers {pos_sum}")
 print(f"Sum of negative numbers {neg_sum}")
